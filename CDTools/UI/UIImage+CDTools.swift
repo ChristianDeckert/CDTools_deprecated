833d6741c59c86e94d41ee10110577b614a3401e
Christian Deckert <christian.deckert@x-root.de>
2016-12-08 09:38:41 +0100

    Enhancements for UIImage extension

--- CDTools/UI/UIImage+CDTools.swift
+++ CDTools/UI/UIImage+CDTools.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Foundation
 
+typealias CoreImage = CIImage
+
 public extension UIImage {
     
     public func averageColor() -> UIColor? {
@@ -35,4 +37,67 @@ public extension UIImage {
         }
         return nil
     }
+    
+    public func darken(withColor color: UIColor = UIColor.blackColor()) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale);
+        guard let cgImage = self.CGImage else {
+            return self
+        }
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return self
+        }
+        
+        let area = CGRectMake(0, 0, self.size.width, self.size.height)
+        CGContextScaleCTM(context, 1, -1)
+        CGContextTranslateCTM(context, 0, -area.size.height);
+        
+        CGContextSaveGState(context);
+        CGContextClipToMask(context, area, cgImage);
+        
+        color.set()
+        
+        CGContextFillRect(context, area);
+        CGContextRestoreGState(context);
+        CGContextSetBlendMode(context, CGBlendMode.Multiply);
+        
+        CGContextDrawImage(context, area, cgImage);
+        
+        let darkenedImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return darkenedImage
+        
+    }
+    
+    
+    public func brighten(byScale brightness: CGFloat = 0.5) -> UIImage? {
+        guard let inputCgImage = self.CGImage else {
+            return self
+        }
+        
+        let inputImage = CoreImage(CGImage: inputCgImage)
+        
+        guard let filter = CIFilter(name: "CIColorControls") else {
+            return self
+        }
+        
+        let context = CIContext.init(options: nil)
+        
+        filter.setValue(inputImage, forKey: kCIInputImageKey)
+        filter.setValue(brightness, forKey: kCIInputBrightnessKey)
+        
+        guard let outputImage = filter.outputImage else {
+            return self
+        }
+        
+        guard let cgImage = context.createCGImage(outputImage, fromRect: outputImage.extent) else {
+            return self
+        }
+        
+        let brighterImage = UIImage(CGImage: cgImage)
+        return brighterImage
+        
+    }
+    
 }
